bbbf0a670c976a12a0226e38cd18711c070e2472
Frank Martinez <frankm-7@tamu.edu>
2022-10-18 15:25:48 -0500

    added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # import Flask
 from flask import Flask, render_template, redirect
+from flask.helpers import send_from_directory
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
