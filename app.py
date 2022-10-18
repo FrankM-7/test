835729151bc0a454f60fd2e6e734675058edd061
Frank Martinez <frankm-7@tamu.edu>
2022-10-18 15:28:16 -0500

    fix

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 # import Flask
 from flask import Flask, render_template, redirect
 from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
+
+app = Flask(__name__ ,static_folder='frontend/build',static_url_path='')
+cors = CORS(app)
 
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run()
